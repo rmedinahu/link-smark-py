0bc4533bd89bfe27dd4d9302f807dfae735e6bed
rmedinahu <rmedina@nmhu.edu>
2017-04-07 16:46:08 -0600

    project prep done

--- link_smark_app/views.py
+++ link_smark_app/views.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView, ListView
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name='home.html'
