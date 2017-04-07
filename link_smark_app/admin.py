263fa7576d26f35b11360d865efc2788d574be67
rmedinahu <rmedina@nmhu.edu>
2017-04-07 12:58:21 -0600

    admin registrations added

--- link_smark_app/admin.py
+++ link_smark_app/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Posts, Bookmarks, Tag, TaggedBookmark, BookmarkComments
+
+admin.site.register(Posts)
+admin.site.register(Bookmarks)
+admin.site.register(Tag)
+admin.site.register(TaggedBookmark)
+admin.site.register(BookmarkComments)
